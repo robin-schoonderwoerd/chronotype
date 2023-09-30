a061f777886bb21f873b421b60d7681b7cc46342
Robin <robin_schoonderwoerd@hotmail.com>
2023-09-30 21:48:15 +0200

    Working calculation with two-binded output.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,9 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-  time = {hour: 14, minute: 30};
-  time2 = {hour: 15, minute: 30};
-  midsleep = {hour: 1, minute: 1}
+  time_sleep_vac = {hour: 23, minute: 30};
+  time_wake_vac = {hour: 7, minute: 30};
+  time_sleep_vac_h = (
+    this.time_sleep_vac.hour + 
+    this.time_sleep_vac.minute / 60
+  )
+  time_wake_vac_h = (
+    this.time_wake_vac.hour + 
+    this.time_wake_vac.minute / 60
+  )
+  midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24
+
+
+  update() {
+  this.time_sleep_vac_h = (
+    this.time_sleep_vac.hour + 
+    this.time_sleep_vac.minute / 60
+    )
+  this.time_wake_vac_h = (
+    this.time_wake_vac.hour + 
+    this.time_wake_vac.minute / 60
+    )
+  this.midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24
+  }
+
 
 
   
