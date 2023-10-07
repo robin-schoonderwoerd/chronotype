8872c4156c8435a54a03759698f9b40bc8c9e0a5
Robin <robin_schoonderwoerd@hotmail.com>
2023-10-07 21:53:11 +0200

    Fixed with images. First version of layout finished.

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,32 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent {
-  time_sleep_vac = {hour: 23, minute: 30};
-  time_wake_vac = {hour: 7, minute: 30};
-  time_sleep_vac_h = (
-    this.time_sleep_vac.hour + 
-    this.time_sleep_vac.minute / 60
-  )
-  time_wake_vac_h = (
-    this.time_wake_vac.hour + 
-    this.time_wake_vac.minute / 60
-  )
-  midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24
-
-
-  update() {
-  this.time_sleep_vac_h = (
-    this.time_sleep_vac.hour + 
-    this.time_sleep_vac.minute / 60
-    )
-  this.time_wake_vac_h = (
-    this.time_wake_vac.hour + 
-    this.time_wake_vac.minute / 60
-    )
-  this.midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24
-  }
-
-
-
-  
 }
--- src/app/components/main-box/main-box.component.ts
+++ src/app/components/main-box/main-box.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class MainBoxComponent {
 
+
+  // Input
+  time_sleep_vac = {hour: 23, minute: 30};
+  time_wake_vac = {hour: 7, minute: 30};
+  time_sleep_vac_h = (
+    this.time_sleep_vac.hour + 
+    this.time_sleep_vac.minute / 60
+  )
+  time_wake_vac_h = (
+    this.time_wake_vac.hour + 
+    this.time_wake_vac.minute / 60
+  )
+  midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24;
+  chronotype = "afternoon"
+
+
+  update() {
+  this.time_sleep_vac_h = (
+    this.time_sleep_vac.hour + 
+    this.time_sleep_vac.minute / 60
+    )
+  this.time_wake_vac_h = (
+    this.time_wake_vac.hour + 
+    this.time_wake_vac.minute / 60
+    )
+  this.midsleephour = (this.time_sleep_vac_h + ((this.time_wake_vac_h + 24 - this.time_sleep_vac_h) % 24) / 2) % 24
+  
+  if (this.midsleephour >= 0 && this.midsleephour < 3) {
+    this.chronotype = "morning";
+  } else if (this.midsleephour >= 3 && this.midsleephour < 6) {
+    this.chronotype = "afternoon";
+  } else if (this.midsleephour >= 6 && this.midsleephour < 9) {
+    this.chronotype = "evening";
+  } else {
+    this.chronotype = "night";
+  } 
+
+
+}
 }
